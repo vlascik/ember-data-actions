23256b97927ad9dd5b09eb5f7071fe6d49a8aec0
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-01-13 17:57:13 -0800

    Update ember-data-actions.js

--- app/initializers/ember-data-actions.js
+++ app/initializers/ember-data-actions.js
@@ -3,7 +3,7 @@ import StoreMixin from 'ember-data-actions/mixins/store';
 export default {
   name: 'ember-data-actions',
   after: 'store',
-  initialize(container) {
+  initialize(instance) {
     var store = instance.__container__.lookup("service:store");
     store.reopen(StoreMixin);
   }
